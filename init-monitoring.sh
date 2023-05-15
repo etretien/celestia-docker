673b4233b882bf8768c7e33b03afe2e5e7c407f7
sorh <digitaldoom@yandex.ru>
2023-05-15 19:13:27 +0300

    fix dir name

--- init-monitoring.sh
+++ init-monitoring.sh
@@ -5,7 +5,7 @@ set -xeu
 mkdir -p volumes/monitoring-prometheus
 mkdir -p volumes/monitoring-grafana
 sudo chown 65534 volumes/monitoring-prometheus
-sudo chown 472 volumes/monitoring-grafan
+sudo chown 472 volumes/monitoring-grafana
 
 stat exporter.env || touch exporter.env
 docker compose -f compose-monitoring.yaml up -d
