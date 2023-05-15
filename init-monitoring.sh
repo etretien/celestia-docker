eaee7fedab8e4e9aa2e9da54416a7994cdf17b89
sorh <digitaldoom@yandex.ru>
2023-05-15 17:14:21 +0300

    edit README.me, info about exporter config
    fix container name in celestia-app

--- init-monitoring.sh
+++ init-monitoring.sh
@@ -6,4 +6,6 @@ mkdir -p volumes/monitoring-prometheus
 mkdir -p volumes/monitoring-grafana
 sudo chown 65534 volumes/monitoring-prometheus
 sudo chown 472 volumes/monitoring-grafan
+
+stat exporter.env || touch exporter.env
 docker compose -f compose-monitoring.yaml up -d
